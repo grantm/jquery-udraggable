a5a6d7664570e230bcd66ed7c8b74d1ad027aee8
Grant McLean <grant@mclean.net.nz>
2013-09-14 12:39:07 +1200

    various formatting 'fixes' suggested by JSHint

--- jquery.udraggable.js
+++ jquery.udraggable.js
@@ -35,19 +35,19 @@
         var that = this;
         this.el  = el;
         this.$el = $(el);
-        this.options = $.extend({}, $.fn.udraggable.defaults, options)
+        this.options = $.extend({}, $.fn.udraggable.defaults, options);
         this.positionElement  = this.options.positionElement  || this.positionElement;
         this.getStartPosition = this.options.getStartPosition || this.getStartPosition;
         this.updatePositionFrameHandler = function() {
             delete that.queuedUpdate;
             var pos = that.ui.position;
-            that.positionElement(that.$el, that.started, pos.left, pos.top)
-            if(that.options.dragUpdate) {
+            that.positionElement(that.$el, that.started, pos.left, pos.top);
+            if (that.options.dragUpdate) {
                 that.options.dragUpdate.apply(that.el, [that.ui]);
             }
         };
         this.queuePositionUpdate = function() {
-            if(!that.queuedUpdate) {
+            if (!that.queuedUpdate) {
                 that.queuedUpdate = window.requestAnimationFrame(that.updatePositionFrameHandler);
             }
         };
@@ -56,16 +56,16 @@
 
     UDraggable.prototype = {
 
-        constructor: UDraggable
+        constructor: UDraggable,
 
-        ,init: function() {
+        init: function() {
             var that = this;
             this.started = false;
             this.normalisePosition();
             var $target = this.options.handle ?
                           this.$el.find( this.options.handle ) :
                           this.$el;
-            if(this.options.longPress) {
+            if (this.options.longPress) {
                 $target
                     .on('uheldstart.uheldd', function(e) { that.start(e); })
                     .on('uheldmove.uheldd',  function(e) { that.move(e);  })
@@ -77,33 +77,35 @@
                     .on('udragmove',  function(e) { that.move(e);  })
                     .on('udragend',   function(e) { that.end(e);   });
             }
-        }
+        },
 
-        ,option: function() {
+        option: function() {
             var name;
-            if( arguments.length === 0 ) {
+            if (arguments.length === 0) {
                 return this.options;
             }
-            if( arguments.length === 2 ) {
+            if (arguments.length === 2) {
                 this.options[ arguments[0] ] = arguments[1];
                 return;
             }
-            if( arguments.length === 1 ) {
-                if( typeof arguments[0] === 'string' ) {
+            if (arguments.length === 1) {
+                if (typeof arguments[0] === 'string') {
                     return this.options[ arguments[0] ];
                 }
-                if( typeof arguments[0] === 'object' ) {
+                if (typeof arguments[0] === 'object') {
                     for(name in arguments[0]) {
-                        this.options[name] = arguments[0][name]
+                        if (arguments[0].hasOwnProperty(name)) {
+                            this.options[name] = arguments[0][name];
+                        }
                     }
                 }
             }
-            if(this.options.containment) {
+            if (this.options.containment) {
                 this._initContainment();
             }
-        }
+        },
 
-        ,normalisePosition: function() {
+        normalisePosition: function() {
             var pos = this.$el.position();
             this.$el.css({
                 position: 'absolute',
@@ -112,9 +114,9 @@
                 right: 'auto',
                 bottom: 'auto'
             });
-        }
+        },
 
-        ,start: function(e) {
+        start: function(e) {
             var start = this.getStartPosition(this.$el);
             this._initContainment();
             this.ui = {
@@ -123,119 +125,119 @@
                 originalPosition: { top: start.y, left: start.x},
                 position:         { top: start.y, left: start.x},
             };
-            if(this.options.longPress) {
+            if (this.options.longPress) {
                 this._start(e);
             }
             return this._stopPropagation(e);
-        }
+        },
 
-        ,move: function(e) {
-            if(!this.started && !this._start(e)) {
+        move: function(e) {
+            if (!this.started && !this._start(e)) {
                 return;
             }
             var delta_x = e.px_current_x - e.px_start_x;
             var delta_y = e.px_current_y - e.px_start_y;
             var axis = this.options.axis;
-            if(axis  &&  axis === "x") {
+            if (axis  &&  axis === "x") {
                 delta_y = 0;
             }
-            if(axis  &&  axis === "y") {
+            if (axis  &&  axis === "y") {
                 delta_x = 0;
             }
             var cur = {
                 left: this.ui.originalPosition.left,
                 top:  this.ui.originalPosition.top
             };
-            if( !axis  ||  (axis === "x") ) {
+            if (!axis  ||  (axis === "x")) {
                 cur.left += delta_x;
             }
-            if( !axis  ||  (axis === "y") ) {
+            if (!axis  ||  (axis === "y")) {
                 cur.top += delta_y;
             }
             this._applyGrid(cur);
             this._applyContainment(cur);
             var pos = this.ui.position;
-            if( (cur.top !== pos.top)  ||  (cur.left !== pos.left) ) {
+            if ((cur.top !== pos.top)  ||  (cur.left !== pos.left)) {
                 this.ui.position.left = cur.left;
                 this.ui.position.top  = cur.top;
                 this.ui.offset.left   = cur.left;
                 this.ui.offset.top    = cur.top;
-                if(this.options.drag) {
+                if (this.options.drag) {
                     this.options.drag.apply(this.el, [e, this.ui]);
                 }
                 this.queuePositionUpdate();
             }
             return this._stopPropagation(e);
-        }
+        },
 
-        ,end: function(e) {
-            if(this.started || this._start(e)) {
+        end: function(e) {
+            if (this.started || this._start(e)) {
                 this.$el.removeClass("udraggable-dragging");
                 this.started = false;
-                if(this.queuedUpdate) {
+                if (this.queuedUpdate) {
                     window.cancelAnimationFrame(this.queuedUpdate);
                 }
                 this.updatePositionFrameHandler();
-                if(this.options.stop) {
+                if (this.options.stop) {
                     this.options.stop.apply(this.el, [e, this.ui]);
                 }
             }
             return this._stopPropagation(e);
-        }
+        },
 
         // helper methods
 
-        ,_stopPropagation: function(e) {
+        _stopPropagation: function(e) {
             e.stopPropagation();
             e.preventDefault();
             return false;
-        }
+        },
 
-        ,_start: function(e) {
-            if( !this._mouseDistanceMet(e) || !this._mouseDelayMet(e) ) {
+        _start: function(e) {
+            if (!this._mouseDistanceMet(e) || !this._mouseDelayMet(e)) {
                 return;
             }
             this.started = true;
             this.queuePositionUpdate();
-            if(this.options.start) {
+            if (this.options.start) {
                 this.options.start.apply(this.el, [e, this.ui]);
             }
             this.$el.addClass("udraggable-dragging");
             return true;
-        }
+        },
 
-        ,_mouseDistanceMet: function(e) {
+        _mouseDistanceMet: function(e) {
             return max(
                 Math.abs(e.px_start_x - e.px_current_x),
                 Math.abs(e.px_start_y - e.px_current_y)
             ) >= this.options.distance;
-        }
+        },
 
-        ,_mouseDelayMet: function(e) {
+        _mouseDelayMet: function(e) {
             return e.ms_elapsed > this.options.delay;
-        }
+        },
 
-        ,_initContainment: function() {
+        _initContainment: function() {
             var o = this.options;
             var $c, ce;
 
-            if( !o.containment ) {
+            if (!o.containment) {
                 this.containment = null;
                 return;
             }
 
-            if( o.containment.constructor === Array ) {
+            if (o.containment.constructor === Array) {
                 this.containment = o.containment;
                 return;
             }
 
-            if( o.containment === "parent" ) {
+            if (o.containment === "parent") {
                 o.containment = this.$el.offsetParent();
             }
 
             $c = $( o.containment );
             ce = $c[ 0 ];
-            if( !ce ) {
+            if (!ce) {
                 return;
             }
 
@@ -245,33 +247,33 @@
                 $c.innerWidth() - this.$el.outerWidth(),
                 $c.innerHeight() - this.$el.outerHeight(),
             ];
-        }
+        },
 
-        ,_applyGrid: function(cur) {
-            if(this.options.grid) {
+        _applyGrid: function(cur) {
+            if (this.options.grid) {
                 var gx = this.options.grid[0];
                 var gy = this.options.grid[1];
                 cur.left = floor( (cur.left + gx / 2) / gx ) * gx;
                 cur.top  = floor( (cur.top  + gy / 2) / gy ) * gy;
             }
-        }
+        },
 
-        ,_applyContainment: function(cur) {
+        _applyContainment: function(cur) {
             var cont = this.containment;
-            if(cont) {
+            if (cont) {
                 cur.left = min( max(cur.left, cont[0]), cont[2] );
                 cur.top  = min( max(cur.top,  cont[1]), cont[3] );
             }
-        }
+        },
 
-        ,getStartPosition: function($el) {
+        getStartPosition: function($el) {
             return {
                 x: parseInt($el.css('left'), 10) || 0,
                 y: parseInt($el.css('top'),  10) || 0
             };
-        }
+        },
 
-        ,positionElement: function($el, dragging, left, top) {
+        positionElement: function($el, dragging, left, top) {
             $el.css({ left: left, top: top });
         }
 
@@ -286,13 +288,13 @@
         this.each(function () {
             var $this = $(this);
             var data = $this.data('udraggable');
-            if(!data) {
+            if (!data) {
                 data = new UDraggable(this, option);
                 $this.data('udraggable', data);
             }
-            if(typeof option === 'string') {  // option is a method - call it
+            if (typeof option === 'string') {  // option is a method - call it
                 var result = data[option].apply(data, args);
-                if(result !== undefined) {
+                if (result !== undefined) {
                     results.push( result );
                 }
             }
@@ -301,14 +303,14 @@
     };
 
     $.fn.udraggable.defaults = {
-         axis:        null
-        ,delay:       0
-        ,distance:    0
-        ,longPress:   false
-        // callbacks
-        ,drag:        null
-        ,start:       null
-        ,stop:        null
+         axis:        null,
+         delay:       0,
+         distance:    0,
+         longPress:   false,
+         // callbacks
+         drag:        null,
+         start:       null,
+         stop:        null
     };
 
 
